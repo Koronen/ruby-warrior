5dd265c6909554b3847c4e0c9c23b2d7ccb7daea
Victor Koronen <koronen@kth.se>
2012-01-07 20:02:39 +0100

    Complete level 3

--- player.rb
+++ player.rb
@@ -1,9 +1,15 @@
 class Player
+  MAX_HEALTH = 20
+
   def play_turn(warrior)
     if warrior.feel.enemy?
       warrior.attack!
     else
-      warrior.walk!
+      if warrior.health < MAX_HEALTH
+        warrior.rest!
+      else
+        warrior.walk!
+      end
     end
   end
 end
